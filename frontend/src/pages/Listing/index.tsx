a5e360027836b84ae5060b5007a6191e9a5a644d
Lucas Couto <loc.unb@gmail.com>
2022-03-08 20:45:47 -0300

    Create MovieStars component

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,13 @@
+import MovieStars from "components/MovieStars";
 import Pagination from "components/Pagination";
 
 function Listing() {
-  return <Pagination />;
+  return (
+    <>
+      <Pagination />
+      <MovieStars />
+    </>
+  );
 }
 
 export default Listing;
